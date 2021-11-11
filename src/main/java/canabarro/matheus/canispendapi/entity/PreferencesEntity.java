32a8af5a36408305ce8d4c2df5c700da7005e8e7
CanabarroMatheus <matheus.canabarro@dbccompany.com.br>
2021-11-11 17:33:54 -0300

    fix(PreferencesEntity): Fix PreferencesEntity to add updatedAt attribute.

--- src/main/java/canabarro/matheus/canispendapi/entity/PreferencesEntity.java
+++ src/main/java/canabarro/matheus/canispendapi/entity/PreferencesEntity.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -22,6 +23,9 @@ public class PreferencesEntity {
 
     @Column(name = "SAVING_PERCENTAGE", nullable = false)
     private Double savingPercentage;
+
+    @Column(name = "UPDATED_AT", nullable = false)
+    private LocalDateTime updatedAt;
 
     @ManyToOne
     @JoinColumn(name = "ID_BALANCE", referencedColumnName = "ID_BALANCE")
