1537eb2e097a09e5a3392a0faf980cd3578cffae
CanabarroMatheus <matheus.canabarro@dbccompany.com.br>
2021-11-11 15:52:14 -0300

    First commit.

---
+++ src/main/java/canabarro/matheus/canispendapi/CanISpendApiApplication.java
@@ -0,0 +1,13 @@
+package canabarro.matheus.canispendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanISpendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanISpendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/canabarro/matheus/canispendapi/CanISpendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package canabarro.matheus.canispendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanISpendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
