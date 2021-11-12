6a714cd1b56e68f51bcad3170cc249f8efb8c363
CanabarroMatheus <matheus.canabarro@dbccompany.com.br>
2021-11-12 12:09:15 -0300

    fix(CanISpendAPIApplicationTests): Fix to add @ActiveProfiles set do Development environment.

--- src/test/java/canabarro/matheus/canispendapi/CanISpendApiApplicationTests.java
+++ src/test/java/canabarro/matheus/canispendapi/CanISpendApiApplicationTests.java
@@ -2,8 +2,10 @@ package canabarro.matheus.canispendapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles( value = "development" )
 class CanISpendApiApplicationTests {
 
 	@Test
