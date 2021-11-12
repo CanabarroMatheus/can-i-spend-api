fc715cbfd6531ee430201093a8f351d8a2742838
CanabarroMatheus <matheus.canabarro@dbccompany.com.br>
2021-11-12 17:50:20 -0300

    add(applications): Add Expected Exception.

---
+++ src/main/java/canabarro/matheus/canispendapi/exception/ExpectedException.java
@@ -0,0 +1,7 @@
+package canabarro.matheus.canispendapi.exception;
+
+public class ExpectedException extends Exception {
+    public ExpectedException(String message) {
+        super(message);
+    }
+}
