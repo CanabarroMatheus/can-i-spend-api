9cb70f6fef337622aad72f3fa0a511e487dc8fe3
CanabarroMatheus <matheus.canabarro@dbccompany.com.br>
2021-12-11 03:12:27 -0300

    fix(User): Fix to add UserDetails and login queries.

--- src/main/java/canabarro/matheus/canispendapi/entity/UserEntity.java
+++ src/main/java/canabarro/matheus/canispendapi/entity/UserEntity.java
@@ -1,8 +1,12 @@
 package canabarro.matheus.canispendapi.entity;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Collection;
 
 @Getter
 @Setter
@@ -11,7 +15,7 @@ import javax.persistence.*;
 @Builder
 @Entity
 @Table(name = "USER")
-public class UserEntity {
+public class UserEntity implements Serializable, UserDetails {
     @Id
     @SequenceGenerator(name = "USER_SEQ", sequenceName = "USER_SEQ", allocationSize = 1)
     @GeneratedValue(generator = "USER_SEQ", strategy = GenerationType.SEQUENCE)
@@ -23,4 +27,42 @@ public class UserEntity {
 
     @Column(name = "EMAIL", nullable = false)
     private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/canabarro/matheus/canispendapi/repository/UserRepository.java
+++ src/main/java/canabarro/matheus/canispendapi/repository/UserRepository.java
@@ -4,6 +4,9 @@ import canabarro.matheus.canispendapi.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
 }
